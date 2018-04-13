9d848f6f36d5553ae6217315f5c7689cc725aee3
Jonathan Stanley <jonathanstanley@users.noreply.github.com>
2018-04-13 09:10:02 -0600

    reordering

--- build.sh
+++ build.sh
@@ -9,14 +9,14 @@ sudo perl -p -i -e 's/format=bsd/format=bsd mode=0640 rotate=utc compress file_m
 # CFR §164.306(a)(3)
 defaults write com.apple.CrashReporter DialogType -string "none" #Disable the crash reporter
 
-# CFR §164.308(a)(5)(ii)(A) & §164.310(c)
-sudo softwareupdate -ia #install all software updates
-defaults write com.apple.commerce AutoUpdate -bool true # Turn on app auto-update
-defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1 # Check for software updates daily
-defaults write com.apple.SoftwareUpdate CriticalUpdateInstall -int 1 # Install System data files & security updates
-
 # CFR §164.312(a)(2)(iii)
 sudo pmset -b displaysleep 2 #turn off display after 2 minutes on battery
 sudo pmset -c displaysleep 10 #turn off display after 10 minutes on cable power
 defaults write com.apple.screensaver askForPassword -int 1 #require password when display off
 defaults write com.apple.screensaver askForPasswordDelay -int 10 #delay password requirement 10 seconds
+
+# CFR §164.308(a)(5)(ii)(A) & §164.310(c)
+defaults write com.apple.commerce AutoUpdate -bool true # Turn on app auto-update
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1 # Check for software updates daily
+defaults write com.apple.SoftwareUpdate CriticalUpdateInstall -int 1 # Install System data files & security updates
+sudo softwareupdate -ia #install all software updates
