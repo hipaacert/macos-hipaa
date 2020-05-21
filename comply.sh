adbefc520a0faeacd42b7e80adafe3fe8b923f55
Jonathan Stanley <jonathanstanley@users.noreply.github.com>
2020-05-21 10:46:59 -0400

    Set computer name from serial number

--- comply.sh
+++ comply.sh
@@ -30,3 +30,13 @@ sudo perl -p -i -e 's/format=bsd/format=bsd mode=0640 rotate=utc compress file_m
 # https://www.govinfo.gov/content/pkg/CFR-2018-title45-vol1/xml/CFR-2018-title45-vol1-sec164-312.xml
 # "Implement a mechanism to encrypt [ePHI] whenever deemed appropriate." & "Implement a mechanism to encrypt and decrypt [ePHI]."
 sudo fdesetup enable #Turn on FileVault Full Disk Encryption FIPS 140-2 Compliant https://support.apple.com/en-us/HT207497
+
+# 45 CFR §164.310(d)
+# https://www.govinfo.gov/content/pkg/CFR-2018-title45-vol1/xml/CFR-2018-title45-vol1-sec164-310.xml
+# Maintain a record of the movements of hardware and electronic media and any person responsible therefore.
+FULLSERIAL=$(system_profiler SPHardwareDataType | sed '/^ *Serial Number (system):*/!d;s###;s/ //') # Get the Apple 12 digit serial number
+UNIQUEPARTOFSERIAL=$(echo $FULLSERIAL | cut -c 6-8) # Get the 3 unique digits from the serial number https://www.macrumors.com/2010/04/16/apple-tweaks-serial-number-format-with-new-macbook-pro/
+sudo scutil --set ComputerName $UNIQUEPARTOFSERIAL # Set short name based  on serial number
+sudo scutil --set HostName $UNIQUEPARTOFSERIAL # Set short name based  on serial number
+sudo scutil --set LocalHostName $UNIQUEPARTOFSERIAL # Set short name based  on serial number
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $UNIQUEPARTOFSERIAL # Set short name based  on serial number
